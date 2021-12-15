fd00534a9b0b01f2fd6788e28d79e120f409cd35
evgenim100 <88602379+evgenim100@users.noreply.github.com>
2021-12-15 20:26:40 +0300

    Update les03.py

--- lesson_07/les03.py
+++ lesson_07/les03.py
@@ -2,5 +2,9 @@ print('Введите a')
 a = int(input())
 print('Введите b')
 b = int(input())
-for i in range(a, b+1):
-    print(i)
+if a < b:
+    for i in range(a, b+1):
+        print(i)
+else:
+    for i in range(b, a+1):
+        print(i)
