eface1d9d3b787a6d22f09b5b8b16762fe33151f
evgenim100 <88602379+evgenim100@users.noreply.github.com>
2021-12-15 19:33:56 +0300

    Add files via upload

--- lesson_07/les08.py
+++ lesson_07/les08.py
@@ -1,8 +1,12 @@
 print('Введите n')
 n = int(input())
-max = -10**9
-min = 10**9
-for i in range(0, n):
+max = int(input())
+min = int(input())
+if max < min:
+   min2 = min
+   min = max
+   max = min2
+for i in range(0, n-2):
     num = int(input())
     if max < num:
         max = num
