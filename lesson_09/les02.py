fdce48d46ad5f2798b2cca843da61992aac44fd4
evgenim100 <88602379+evgenim100@users.noreply.github.com>
2021-12-19 20:51:07 +0300

    Update les02.py

--- lesson_09/les02.py
+++ lesson_09/les02.py
@@ -4,9 +4,9 @@ n = int(input())
 for i in range(2, n // 2):
     print(i)
     if n % i == 0:
+        print('NO')
         count += 1
-if count > 0:
-    print('NO')
-else:
+        break
+if count == 0:
     print('YES')
 
